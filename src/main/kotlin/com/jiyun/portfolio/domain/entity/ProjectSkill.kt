beec014f5cc212dd7ffd3a586882bf1ca9845473
jiyun-im-dev <jiyun.im.dev@gmail.com>
2024-12-04 16:14:51 +0900

    연관관계 있는 엔티티 개발

--- src/main/kotlin/com/jiyun/portfolio/domain/entity/ProjectSkill.kt
+++ src/main/kotlin/com/jiyun/portfolio/domain/entity/ProjectSkill.kt
@@ -3,11 +3,19 @@ package com.jiyun.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class ProjectSkill : BaseEntity() {
+class ProjectSkill(project: Project, skill: Skill) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_skill_id")
     var id: Long? = null
+
+    @ManyToOne(targetEntity = Project::class, fetch = FetchType.LAZY) // 프로젝트 당 스킬이 여러 개
+    @JoinColumn(name = "project_id", nullable = false)                // 연관관계 주인은 ProjectSkill 엔티티
+    var project: Project = project
+
+    @ManyToOne(targetEntity = Skill::class, fetch = FetchType.LAZY) // 프로젝트 스킬이 여러 개
+    @JoinColumn(name = "skill_id", nullable = false)                // 연관관계 주인은 ProjectSkill 엔티티
+    var skill: Skill = skill
 
 }
