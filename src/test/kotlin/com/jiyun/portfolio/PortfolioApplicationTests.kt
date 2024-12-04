fa90738f07ac2d908e8e530bbeb02279ea2ccd74
jiyun-im-dev <jiyun.im.dev@gmail.com>
2024-12-04 10:49:40 +0900

    Git과 Github

---
+++ src/test/kotlin/com/jiyun/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jiyun.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
