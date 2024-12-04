3896e17c1e69cb654367d1d3435cec7af3de9d14
jiyun-im-dev <jiyun.im.dev@gmail.com>
2024-12-04 14:22:01 +0900

    연관관계 없는 엔티티 개발

--- src/main/kotlin/com/jiyun/portfolio/domain/entity/Achievement.kt
+++ src/main/kotlin/com/jiyun/portfolio/domain/entity/Achievement.kt
@@ -1,13 +1,31 @@
 package com.jiyun.portfolio.domain.entity
 
 import jakarta.persistence.*
+import java.time.LocalDate
 
 @Entity // JPA에서 이 클래스는 테이블과 매핑되는 엔티티 클래스임을 표시한다
-class Achievement : BaseEntity() { // BaseEntity를 상속
+class Achievement( // 기본 생성자 정의
+    title: String,
+    description: String,
+    achievedDate: LocalDate?,
+    host: String,
+    isActive: Boolean
+) : BaseEntity() { // BaseEntity를 상속
 
     @Id // 이 필드가 PK임을 표시
     @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성 전략 - 데이터베이스(MySQL의 Auto Increment)에 위임
     @Column(name = "achievement_id") // 이름을 생략해도 필드는 캐멀 케이스, DB는 스네이크 케이스로 된 경우 자동으로 매핑할 컬럼을 찾아준다
     var id: Long? = null // 엔티티 생성 시가 아니라 엔티티를 데이터베이스에 저장할 때 DB가 ID를 생성하므로 널을 허용한다
+
+    var title: String = title // 생성자에서 받은 title을 초기값으로 넣어준다
+
+    var description: String = description
+
+    var achievedDate: LocalDate? = achievedDate
+
+    var host: String = host
+
+    // DB에서는 스네이크 케이스로 변환되어 is_active로 들어감
+    var isActive: Boolean = isActive
 
 }
--- src/main/kotlin/com/jiyun/portfolio/domain/entity/BaseEntity.kt
+++ src/main/kotlin/com/jiyun/portfolio/domain/entity/BaseEntity.kt
@@ -1,6 +1,20 @@
 package com.jiyun.portfolio.domain.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
 
 @MappedSuperclass // 이 클래스를 상속받는 엔티티 클래스가 이 클래스의 필드들을 해당하는 테이블의 컬럼과 매핑할 수 있게 해줌
-abstract class BaseEntity
+abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    var createdDateTime: LocalDateTime = LocalDateTime.now()
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    var updatedDateTime: LocalDateTime = LocalDateTime.now()
+
+}
