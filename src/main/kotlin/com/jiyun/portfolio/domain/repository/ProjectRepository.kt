8b9c5471034cc7d3d85f000aaf5c86a1ee9b7cab
jiyun-im-dev <jiyun.im.dev@gmail.com>
2024-12-05 18:11:13 +0900

    리포지토리 성능 개선

--- src/main/kotlin/com/jiyun/portfolio/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/com/jiyun/portfolio/domain/repository/ProjectRepository.kt
@@ -2,9 +2,16 @@ package com.jiyun.portfolio.domain.repository
 
 import com.jiyun.portfolio.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.*
 
 interface ProjectRepository : JpaRepository<Project, Long> {
 
+    // s.skill은 ProjectSkill 엔티티의 skill 필드
+    @Query("select p from Project p left join fetch p.skills s left join fetch s.skill where p.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Project>
+
+    @Query("select p from Project p left join fetch p.details where p.id = :id")
+    override fun findById(id: Long): Optional<Project>
 
 }
