d4b0d2013d5c6d6e270696f7c94e841263d5a11f
jiyun-im-dev <jiyun.im.dev@gmail.com>
2024-12-06 14:33:45 +0900

    프레젠테이션 서비스 개발

--- src/main/kotlin/com/jiyun/portfolio/presentation/service/PresentationService.kt
+++ src/main/kotlin/com/jiyun/portfolio/presentation/service/PresentationService.kt
@@ -1,6 +1,40 @@
 package com.jiyun.portfolio.presentation.service
 
+import com.jiyun.portfolio.presentation.dto.IntroductionDto
+import com.jiyun.portfolio.presentation.dto.LinkDto
+import com.jiyun.portfolio.presentation.dto.ProjectDto
+import com.jiyun.portfolio.presentation.dto.ResumeDto
+import com.jiyun.portfolio.presentation.repository.PresentationRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class PresentationService
+class PresentationService(
+    private val presentationRepository: PresentationRepository
+) {
+
+    @Transactional(readOnly = true)
+    fun getIntroductions(): List<IntroductionDto> {
+        return presentationRepository.getActiveIntroductions().map { IntroductionDto(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getLinks(): List<LinkDto> {
+        return presentationRepository.getActiveLinks().map { LinkDto(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getResume(): ResumeDto {
+        val experiences = presentationRepository.getActiveExperiences()
+        val achievements = presentationRepository.getActiveAchievements()
+        val skills = presentationRepository.getActiveSkills()
+
+        return ResumeDto(experiences, achievements, skills)
+    }
+
+    @Transactional(readOnly = true)
+    fun getProjects(): List<ProjectDto> {
+        return presentationRepository.getActiveProjects().map { ProjectDto(it) }
+    }
+
+}
