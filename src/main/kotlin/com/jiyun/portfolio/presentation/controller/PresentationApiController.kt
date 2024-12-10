ba62aa50fa2e97f01541f01855a18d991b9bf7dd
jiyun-im-dev <jiyun.im.dev@gmail.com>
2024-12-10 17:19:50 +0900

    프레젠테이션 컨트롤러 개발

--- src/main/kotlin/com/jiyun/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/jiyun/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,8 +1,43 @@
 package com.jiyun.portfolio.presentation.controller
 
+import com.jiyun.portfolio.presentation.dto.IntroductionDto
+import com.jiyun.portfolio.presentation.dto.LinkDto
+import com.jiyun.portfolio.presentation.dto.ProjectDto
+import com.jiyun.portfolio.presentation.dto.ResumeDto
+import com.jiyun.portfolio.presentation.service.PresentationService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class PresentationApiController
+class PresentationApiController( // 백->프론트로 데이터 전달 후 클라이언트 사이드 렌더링
+    private val presentationService: PresentationService
+) {
+
+    @GetMapping("/test")
+    fun test(): String {
+        return "ok"
+    }
+
+    @GetMapping("/v1/introductions") // api 컨트롤러엔 보통 버전을 붙인다
+    fun getIntroductions(): List<IntroductionDto> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/links")
+    fun getLinks(): List<LinkDto> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/v1/resume") // api 컨트롤러엔 보통 버전을 붙인다
+    fun getResume(): ResumeDto {
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/v1/projects")
+    fun getProjects(): List<ProjectDto> {
+        return presentationService.getProjects()
+    }
+
+}
--- src/main/kotlin/com/jiyun/portfolio/presentation/controller/PresentationViewController.kt
+++ src/main/kotlin/com/jiyun/portfolio/presentation/controller/PresentationViewController.kt
@@ -1,6 +1,39 @@
 package com.jiyun.portfolio.presentation.controller
 
+import com.jiyun.portfolio.domain.constant.SkillType
+import com.jiyun.portfolio.presentation.service.PresentationService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class PresentationViewController
+class PresentationViewController( // 서버 사이드 렌더링
+    private val presentationService: PresentationService
+) {
+
+    @GetMapping("/test")
+    fun test(): String {
+        return "test"
+    }
+
+    @GetMapping("/") // 인덱스 페이지에는 설명, 링크가 나옴
+    fun index(model: Model): String {
+        model.addAttribute("introductions", presentationService.getIntroductions())
+        model.addAttribute("links", presentationService.getLinks())
+        return "presentation/index"
+    }
+
+    @GetMapping("/resume")
+    fun resume(model: Model): String {
+        model.addAttribute("resume", presentationService.getResume())
+        model.addAttribute("skillTypes", SkillType.values())
+        return "presentation/resume"
+    }
+
+    @GetMapping("/projects")
+    fun projects(model: Model): String {
+        model.addAttribute("projects", presentationService.getProjects())
+        return "presentation/projects"
+    }
+
+}
