1e4db4aeab9840a8ebb1b006a8ec8feaa547b684
jiyun-im-dev <jiyun.im.dev@gmail.com>
2024-12-13 12:03:40 +0900

    인터셉터 개발

--- src/main/kotlin/com/jiyun/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/jiyun/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,6 +1,25 @@
 package com.jiyun.portfolio.presentation.interceptor
 
+import com.jiyun.portfolio.domain.entity.HttpInterface
+import com.jiyun.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
+
+}
